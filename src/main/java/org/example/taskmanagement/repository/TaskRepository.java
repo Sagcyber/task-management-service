36ec1cb718f0d2fc74a50b553da895732d6eb781
Sagcyber <immortelleame@gmail.com>
2026-01-08 12:51:46 +0700

    Add JPA repositories for User and Task

---
+++ src/main/java/org/example/taskmanagement/repository/TaskRepository.java
@@ -0,0 +1,16 @@
+package org.example.taskmanagement.repository;
+
+import org.example.taskmanagement.model.Category;
+import org.example.taskmanagement.model.Task;
+import org.example.taskmanagement.model.TaskStatus;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface TaskRepository extends JpaRepository<Task, Long> {
+    
+    List<Task> findByCategory(Category category);
+    
+    List<Task> findByStatus(TaskStatus status);
+
+}
