853d6f90b44ee9cc89b6cf01ceb60a95b384073b
Sagcyber <immortelleame@gmail.com>
2026-01-09 20:08:50 +0700

    feat: add Liquibase migrations for users and tasks

--- src/main/resources/db/001-create-users-table.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE users (
-                       id BIGSERIAL PRIMARY KEY,
-                       username VARCHAR(255) NOT NULL UNIQUE,
-                       email VARCHAR(255) NOT NULL UNIQUE,
-                       created_at TIMESTAMP NOT NULL
-);
