5ea5a3d9c05d73f17b37b942ba64d148c4ee37d4
Sagcyber <immortelleame@gmail.com>
2026-01-11 16:21:42 +0700

    Configure Redis cache TTL per cache

--- src/main/java/org/example/taskmanagement/config/RedisConfig.java
+++ src/main/java/org/example/taskmanagement/config/RedisConfig.java
@@ -3,16 +3,38 @@ package org.example.taskmanagement.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
 
 import java.time.Duration;
+import java.util.HashMap;
+import java.util.Map;
 
 @Configuration
 public class RedisConfig {
     
     @Bean
-    public RedisCacheConfiguration cacheConfiguration() {
-        return RedisCacheConfiguration.defaultCacheConfig()
-                                      .entryTtl(Duration.ofMinutes(10))
-                                      .disableCachingNullValues();
+    public RedisCacheManager cacheManager(RedisConnectionFactory connectionFactory) {
+        
+        RedisCacheConfiguration defaultConfig =
+                RedisCacheConfiguration.defaultCacheConfig()
+                                       .disableCachingNullValues();
+        
+        Map<String, RedisCacheConfiguration> cacheConfigs = new HashMap<>();
+        
+        cacheConfigs.put(
+                "users",
+                defaultConfig.entryTtl(Duration.ofMinutes(10))
+        );
+        
+        cacheConfigs.put(
+                "tasks",
+                defaultConfig.entryTtl(Duration.ofMinutes(2))
+        );
+        
+        return RedisCacheManager.builder(connectionFactory)
+                                .cacheDefaults(defaultConfig)
+                                .withInitialCacheConfigurations(cacheConfigs)
+                                .build();
     }
 }
